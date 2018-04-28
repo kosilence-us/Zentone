f526ac5edcadd1c647053513dd86b42e3595cf44
jt1992@gmail.com <jt1992@gmail.com>
2018-04-28 17:19:22 +0800

    webpack js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 const debug = process.env.NODE_ENV !== 'production';
@@ -19,33 +18,66 @@ module.exports = {
     path: path.resolve(__dirname, 'public/js'),
     filename: 'bundle.min.js'
   },
-  module: {
-    rules: [
-      /*
-      your other rules for JavaScript transpiling go in here
-      */
-      { // regular css files
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          loader: 'css-loader?importLoaders=1',
-        }),
-      },
-      { // sass / scss loader for webpack
-        test: /\.(sass|scss)$/,
-        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-      }
-    ]
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(sass|scss)$/,
+  //       use: [{
+  //         loader: 'style-loader'
+  //       }, {
+  //         loader: 'css-loader'
+  //       }, {
+  //         loader: 'sass-loader',
+  //         options: {
+  //           includePaths: [
+  //             path.resolve(__dirname, './public/css')
+  //           ]
+  //         }
+  //       }]
+  //     }, {
+  //       test: /\.css$/,
+  //       use: {
+  //         loader: 'style-loader!css-loader'
+  //       }
+  //     }, {
+  //       test: /\.png$/,
+  //       use: {
+  //         loader: 'url-loader?limit=100000'
+  //       }
+  //     }, {
+  //       test: /\.jpg$/,
+  //       use: {
+  //         loader: 'file-loader'
+  //       }
+  //     }, {
+  //       test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+  //       use: {
+  //         loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+  //       }
+  //     }, {
+  //       test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+  //       use: {
+  //         loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+  //       }
+  //     }, {
+  //       test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+  //       use: {
+  //         loader: 'file-loader'
+  //       }
+  //     }, {
+  //       test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+  //       use: {
+  //         loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+  //       }
+  //     }
+  //   ]
+  // },
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery',
       'window.$': 'jquery'
-    }),
-    new ExtractTextPlugin({ // define where to save the file
-      filename: 'public/css/stylesheet.bundle.css',
-      allChunks: true,
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     // new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
